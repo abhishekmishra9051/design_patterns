9421f2d0950354fd4c17799c2255cb6614dcbc62
Abhishek Mishra <abhishekmishra9051@gmail.com>
2023-07-08 16:46:02 +0530

    Demo added

---
+++ singleton/src/main/java/org/example/Demo.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class Demo
+{
+    public static  class Book{
+        private static Book book;
+        private Book(){}
+        public static Book getBook(){
+            if(book == null){
+                synchronized(Book.class){
+                    if(book == null){
+                        book = new Book();
+                    }
+                }
+            }
+            return book;
+        }
+    }
+
+    public static class Copy{
+        private static Copy copy = new Copy();
+        public static Copy getCopy(){
+            return copy;
+        }
+    }
+    public static void main(String[] args){
+        Book book = Book.getBook();
+        System.out.println(book.hashCode());
+        Book book1 = Book.getBook();
+        System.out.println(book1.hashCode());
+        Copy copy = Copy.getCopy();
+        System.out.println(copy.hashCode());
+        Copy copy1 = Copy.getCopy();
+        System.out.println(copy1.hashCode());
+    }
+}
